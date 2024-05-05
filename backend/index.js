6a2ca28af4260ea4caa7973dd3d73040bb58bd01
Vanessa Kasminoff <sunnset07@gmail.com>
2024-05-04 22:03:13 -0700

    Implemented user login and signup capabilities, including authentication and password hashing

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json())
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
