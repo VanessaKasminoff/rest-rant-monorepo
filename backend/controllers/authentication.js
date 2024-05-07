c22310849ee85403d479d7b08cf3c5f45d439879
Vanessa Kasminoff <sunnset07@gmail.com>
2024-05-06 21:06:54 -0700

    Implemented auth with JWT

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require('../models')
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const {User} = db
 
@@ -12,9 +13,36 @@ router.post('/', async (req, res) => {
     if(!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({message: `Could not find a user with the provided username and password`})
     } else {
-        res.json({user})
+        const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId})
+        res.json({user: user, token: result.value})
     }
     console.log(user)
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        //split the auth header into ['Bearer', 'token]
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        //only handle 'Bearer' auth for now
+        if (authenticationMethod == 'Bearer') {
+            //decode JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            //get the logged in user's id from the payload
+            const {id} = result.value
+
+            //find the user object using their id
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
+    } catch {
+        res.json(null)
+    }
 })
 
 module.exports = router
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,23 +17,27 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
-       const response = await fetch(`http://localhost:3030/authentication/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-       })
-       const data = await response.json()
+        try {
+            const response = await fetch(`http://localhost:3030/authentication/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(credentials)
+            })
+            const data = await response.json()
 
-       if (response.status === 200) {
-        setCurrentUser(data.user)
-        history.push(`/`)
-       } else {
-        setErrorMessage(data.message)
-       }
-
-       console.log(data)
+            if (response.status === 200) {
+                setCurrentUser(data.user)
+                localStorage.setItem('token', data.token)
+                history.push(`/`)
+            } else {
+                setErrorMessage(data.message)
+            }
+        } catch (error) {
+            console.error('An error occured:', error)
+            setErrorMessage('An error occured, please try again')
+        }
     }
 
     return (
